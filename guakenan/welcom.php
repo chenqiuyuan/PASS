7462ebb649ec498ecf467769c003a7ddfa7672bb
chenqiuyuan <380133194@qq.com>
2015-04-01 23:46:55 +0800

    submit

---
+++ guakenan/welcom.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>	
+	<meta charset="UTF-8">
+	<title>
+	挂科难
+
+	</title>
+</head>
+
+<body>
+	  <h1>挂科难</h1>
+	  <h5>欢迎来到挂科难，我们旨在为科大学子提供服务，方便考前刷题，不再为资料和资料费烦恼</h5>
+
+<p>登陆您的考试天地</p>
+<form action="judge.php" method="post">
+账号:<input type="text" value="么么哒" name="username"><br/>
+密码:<input type="password"  name="userpwd"><br/>
+<input type="submit" value="登陆"><br/>
+</form>
+<a href="register.php" target="_blank">还没注册？快加入复习大军吧</a>
+
+
+<br/><br/><br/><br/>
+对我们有什么意见，请告诉我们<br/>
+<form action = "mail_complain.php" method="post">
+<textarea name="comment" rows="5" cols="10" ></textarea>
+<input type="submit" value="吐槽"><br/>
+</form>
+
+</body>
+</html>

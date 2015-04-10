ccb906c99d8d9d8e8e2cd2a2ed96661cc2cc1337
陈亚东 <dick-cyd@163.com>
2015-04-10 13:30:22 +0800

    完成了所有科目间的跳转

--- interface/guakenan/register_db.php
+++ interface/guakenan/register_db.php
@@ -18,21 +18,18 @@
 	$sql="INSERT INTO account set username='" . $username . "',userpwd='" . $md5_userpwd . "',status=0;";
 
 	if(mysql_query($sql,$con)){
-		echo "成功";
+		echo "成功</br>";
+		echo "请进入邮箱进行验证~已经实现</br>";
+			//mail_register.php 包含了一大堆代码，欲修改请跳转，并注意两页公用变量名mail（用户名必须用邮箱）
+		include 'mail_register.php';//发送验证邮件
 	}else{
-		echo "失败";
+		echo "失败!</br>";
+		echo "原因：网络不通畅或该账号已经被注册";
 	}
 	mysql_close($con);
-	//mail_register.php 包含了一大堆代码，欲修改请跳转，并注意两页公用变量名mail（用户名必须用邮箱）
-	include 'mail_register.php';//发送验证邮件
+
+
+	
+
 ?>
 
-	<html>
-	<body>
-		<br/>
-		请进入邮箱进行验证~已经实现
-		<a href="welcom.php">返回首页</a>
-		<br/>
-
-	</body>
-	</html>
